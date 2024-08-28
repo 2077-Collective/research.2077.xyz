ad2fbae9db39348cd4909930268b54788015c9f4
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-08-28 15:31:08 -0300

    refactor: footer style and component

---
+++ src/components/icons/Twitter.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let size = 40 // Default size if not provided
+</script>
+
+<svg
+    class="hover:scale-110 transition-transform duration-300"
+    {size}
+    width={size}
+    height={size * 1.16}
+    viewBox="0 0 74 86"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+        d="M73.4297 64.0489V21.6763L36.7593 0.48999L0.0912781 21.6763V64.0489L36.7593 85.2352L73.4297 64.0489Z"
+        fill="white"
+        class="hover:fill-[#C6FF50]"
+    ></path>
+    <path
+        d="M41.2296 40.4741L53.2337 25.5967H47.8758L38.7137 36.9592L30.5854 25.5967H18.9213L32.5312 44.6184L19.7407 60.467H25.1003L35.047 48.1403L43.871 60.467H55.5351L41.2296 40.4741ZM37.08 45.6175L34.5606 42.0973L24.7795 28.4334H28.807L36.7016 39.4472L39.2175 42.9691L49.71 57.6303H45.6825L37.08 45.6175Z"
+        fill="#2B2D2D"
+    ></path>
+</svg>
