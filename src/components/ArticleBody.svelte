4442c2f61cf9f64aff1475d050e5de297ebd6195
2077collective <173377129+2077collective@users.noreply.github.com>
2024-09-09 08:45:06 +0100

    Revert "Update staging"

--- src/components/ArticleBody.svelte
+++ src/components/ArticleBody.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
+    import { onMount } from 'svelte'
+
     export let content: string
 </script>
 
 <div
-    class="
+    class="text-red
     max-w-screen-lg m-auto flex flex-col gap-y-4 mb-32 dark:text-white
-    [&>p>a]:text-green-900 [&>p>a]:font-bold
+    [&>p>a]:text-green-900 [&>p>a]:font-semibold
     [&>h1]:text-3xl [&>h2]:text-2xl [&>h3]:text-xl [&>h4]:text-lg [&>h1]:font-semibold
     [&>h2]:font-semibold [&>h3]:font-semibold [&>h4]:font-semibold
     [&>ul]:list-disc [&>ol]:list-decimal [&>ul]:pl-6 [&>ol>li]:pl-4 [&>ol]:pl-6
-    [&>ul>li>span>a]:text-green-900 [&>ul>li>span>a]:font-bold
     [&>ul>li>ul]:list-disc [&>ul>li>ul]:pl-8 [&>ul>li>ul>li>ul]:list-disc [&>ul>li>ul>li>ul]:pl-8
     [&>ul>li>ul>li>ul>li>ul]:list-disc [&>ul>li>ul>li>ul>li>ul]:pl-8
     [&>p>em]:text-gray-200 [&>p>em]:text-center [&>p>img]:mx-auto [&>p>img]:mt-4 [&>p:has(img)]:text-center
